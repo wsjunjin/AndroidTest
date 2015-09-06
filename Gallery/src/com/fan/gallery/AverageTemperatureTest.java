2532599d0170a5ab820e5237e411dd26c70f1f5b
wsjunjin <wsjunjin@163.com>
2015-09-06 11:04:46 +0800

    根据采样Cpu画折线图

---
+++ Gallery/src/com/fan/gallery/AverageTemperatureTest.java
@@ -0,0 +1,201 @@
+package com.fan.gallery;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.chart.AbstractChart;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.TimeSeries;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.SimpleSeriesRenderer;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Align;
+
+public class AverageTemperatureTest extends AbstractChart {
+
+	public String getName() {
+		return "Average temperatureCPU";
+	}
+
+	public String getDesc() {
+		return "The average temperature in 4 Greek islands (line chart)";
+	}
+
+	/**
+	 * Executes the chart demo.
+	 * 
+	 * @param context
+	 *            the context
+	 * @return the built intent
+	 */
+	public Intent execute(Context context) {
+		// 每个item的title
+		String[] titles = new String[] { "Crete", "Corfu", "Thassos",
+				"Skiathos" };
+		// x轴的值
+		List<double[]> x = new ArrayList<double[]>();
+		for (int i = 0; i < titles.length; i++) {
+			x.add(new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 });
+		}
+		// y轴的值
+		List<double[]> values = new ArrayList<double[]>();
+		values.add(new double[] { 12.3, 12.5, 13.8, 16.8, 20.4, 24.4, 26.4,
+				26.1, 23.6, 20.3, 17.2, 13.9 });
+		values.add(new double[] { 10, 10, 12, 15, 20, 24, 26, 26, 23, 18, 14,
+				11 });
+		values.add(new double[] { 5, 5.3, 8, 12, 17, 22, 24.2, 24, 19, 15, 9, 6 });
+		values.add(new double[] { 9, 10, 11, 15, 19, 23, 26, 25, 22, 18, 13, 10 });
+		int[] colors = new int[] { Color.BLUE, Color.GREEN, Color.CYAN,
+				Color.YELLOW };
+		// 点的样式
+		PointStyle[] styles = new PointStyle[] { PointStyle.CIRCLE,
+				PointStyle.DIAMOND, PointStyle.TRIANGLE, PointStyle.SQUARE };
+		XYMultipleSeriesRenderer renderer = buildRenderer(colors, styles);
+		int length = renderer.getSeriesRendererCount();
+		// 点是空心还是实心
+		for (int i = 0; i < length; i++) {
+			((XYSeriesRenderer) renderer.getSeriesRendererAt(i))
+					.setFillPoints(true);
+		}
+
+		// 图表部分的背景颜色
+		renderer.setBackgroundColor(Color.parseColor("#f3f3f3"));
+		renderer.setApplyBackgroundColor(true);
+		// 图表与屏幕四边的间距颜色
+		renderer.setMarginsColor(Color.argb(0, 0xF3, 0xF3, 0xF3));
+		renderer.setChartTitleTextSize(30);
+		renderer.setAxisTitleTextSize(25);
+		// renderer.setLegendHeight(50);
+		// 图例文字的大小
+		renderer.setLegendTextSize(20);
+		renderer.setMargins(new int[] { 50, 50, 50, 30 });
+		// x、y轴上刻度颜色
+		renderer.setXLabelsColor(Color.BLACK);
+		renderer.setYLabelsColor(0, Color.BLACK);
+
+		// 最后两个参数代表轴的颜色和轴标签的颜色
+		setChartSettings(renderer, "Average temperature", "Month",
+				"Temperature", 0.5, 12.5, -10, 40, Color.BLACK, Color.BLACK);
+		// 轴上数字的数量
+		renderer.setXLabels(12);
+		renderer.setYLabels(10);
+		// 是否显示网格
+		renderer.setShowGrid(true);
+		// x或y轴上数字的方向，相反的。
+		renderer.setXLabelsAlign(Align.RIGHT);
+		renderer.setYLabelsAlign(Align.RIGHT);
+		// 有问题的方法
+		// renderer.setZoomButtonsVisible(true);
+		// renderer.setPanLimits(new double[] { -10, 20, -10, 40 });
+		// renderer.setZoomLimits(new double[] { -10, 20, -10, 40 });
+
+		Intent intent = ChartFactory.getLineChartIntent(context,
+				buildDataset(titles, x, values), renderer,
+				"Average temperature");
+		return intent;
+	}
+
+	  protected XYMultipleSeriesDataset buildDataset(String[] titles, List<double[]> xValues,
+		      List<double[]> yValues) {
+		    XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+		    addXYSeries(dataset, titles, xValues, yValues, 0);
+		    return dataset;
+		  }
+	  
+	  public void addXYSeries(XYMultipleSeriesDataset dataset, String[] titles, List<double[]> xValues,
+		      List<double[]> yValues, int scale) {
+		    int length = titles.length;
+		    for (int i = 0; i < length; i++) {
+		      XYSeries series = new XYSeries(titles[i], scale);
+		      double[] xV = xValues.get(i);
+		      double[] yV = yValues.get(i);
+		      int seriesLength = xV.length;
+		      for (int k = 0; k < seriesLength; k++) {
+		        series.add(xV[k], yV[k]);
+		      }
+		      dataset.addSeries(series);
+		    }
+		  }
+	  protected void setRenderer(XYMultipleSeriesRenderer renderer, int[] colors, PointStyle[] styles) {
+		    renderer.setAxisTitleTextSize(16);
+		    renderer.setChartTitleTextSize(20);
+		    renderer.setLabelsTextSize(15);
+		    renderer.setLegendTextSize(15);
+		    renderer.setPointSize(5f);
+		    renderer.setMargins(new int[] { 20, 30, 15, 20 });
+		    int length = colors.length;
+		    for (int i = 0; i < length; i++) {
+		      XYSeriesRenderer r = new XYSeriesRenderer();
+		      r.setColor(colors[i]);
+		      r.setPointStyle(styles[i]);
+		      renderer.addSeriesRenderer(r);
+		    }
+		  }
+
+	  protected XYMultipleSeriesRenderer buildRenderer(int[] colors, PointStyle[] styles) {
+		    XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+		    setRenderer(renderer, colors, styles);
+		    return renderer;
+		  }
+
+	@Override
+	public void draw(Canvas arg0, int arg1, int arg2, int arg3, int arg4,
+			Paint arg5) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawLegendShape(Canvas arg0, SimpleSeriesRenderer arg1,
+			float arg2, float arg3, int arg4, Paint arg5) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getLegendShapeWidth(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	  protected XYMultipleSeriesDataset buildDateDataset(String[] titles, List<Date[]> xValues,
+		      List<double[]> yValues) {
+		    XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+		    int length = titles.length;
+		    for (int i = 0; i < length; i++) {
+		      TimeSeries series = new TimeSeries(titles[i]);
+		      Date[] xV = xValues.get(i);
+		      double[] yV = yValues.get(i);
+		      int seriesLength = xV.length;
+		      for (int k = 0; k < seriesLength; k++) {
+		        series.add(xV[k], yV[k]);
+		      }
+		      dataset.addSeries(series);
+		    }
+		    return dataset;
+		  }
+	  protected void setChartSettings(XYMultipleSeriesRenderer renderer, String title, String xTitle,
+		      String yTitle, double xMin, double xMax, double yMin, double yMax, int axesColor,
+		      int labelsColor) {
+		    renderer.setChartTitle(title);
+		    renderer.setXTitle(xTitle);
+		    renderer.setYTitle(yTitle);
+		    renderer.setXAxisMin(xMin);
+		    renderer.setXAxisMax(xMax);
+		    renderer.setYAxisMin(yMin);
+		    renderer.setYAxisMax(yMax);
+		    renderer.setAxesColor(axesColor);
+		    renderer.setLabelsColor(labelsColor);
+		  }
+}
+
+
---
+++ Gallery/src/com/fan/gallery/Utils.java
@@ -0,0 +1,152 @@
+package com.fan.gallery;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import android.R.integer;
+import android.os.Environment;
+import android.renderscript.Script.KernelID;
+import android.util.Log;
+
+public class Utils {
+	private static final String TAG = "write info:";
+	public static final String LOG_FILE_NAME = "log1.txt";
+	public static final String CPU_FILE_NAME = "log2.txt";
+	public static final int DELAYTIME = 1;
+    /** 
+     * 判断SDCard是否存在 [当没有外挂SD卡时，内置ROM也被识别为存在sd卡] 
+     *  
+     * @return 
+     */  
+    public static boolean isSdCardExist() {  
+        return Environment.getExternalStorageState().equals(  
+                Environment.MEDIA_MOUNTED);  
+    }
+    
+    /** 
+     * 获取SD卡根目录路径 
+     *  
+     * @return 
+     */  
+    public static String getSdCardPath() {  
+        boolean exist = isSdCardExist();  
+        String sdpath = "";  
+        if (exist) {  
+            sdpath = Environment.getExternalStorageDirectory()  
+                    .getAbsolutePath();  
+        } else {  
+            sdpath = "SD卡不存在";  
+        }  
+        return sdpath;  
+      
+    } 
+    
+    /**
+     * 写日志
+     * @param info 
+     *        需要写入的日志信息
+     * @param flag  
+     *        false 重新建立文件并写日志
+     *        true 添加日志
+     */  
+    public static void writeLog(String fileName,String info,Boolean flag){
+    	if (isSdCardExist()) {
+            File file = new File(Environment.getExternalStorageDirectory(), fileName);          
+    		try { 
+    			OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file,flag),"gbk");
+    			BufferedWriter bw = new BufferedWriter(writer);  		    			 
+    		    SimpleDateFormat sDateFormat = new SimpleDateFormat("[yyyy-MM-dd hh:mm:ss:SSS]");       
+    		    String date = sDateFormat.format(new java.util.Date());
+    		    bw.write(date + info + "\r\n");  
+    		    bw.flush();  
+    	        Log.i(TAG, "写日志信息成功");
+    		} catch (FileNotFoundException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		} catch (IOException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}  
+     
+        }
+	}
+    
+	public static double getPercent(float fraction) {
+		fraction *= 100;
+	    double d = Math.round(fraction);
+	    return d;
+	}
+	
+    /** 
+     * 读日志
+     */ 
+    public static String readLog(String fileName) {  
+	    String str = "";
+    	if (isSdCardExist()) {
+			try {
+	    		File file = new File(Environment.getExternalStorageDirectory(), fileName);     
+	    		InputStreamReader reader = new InputStreamReader(new FileInputStream(file), "gbk");
+	    	    BufferedReader br = new BufferedReader(reader);  
+	    	    String readline = "";    
+	    	    StringBuffer sb = new StringBuffer();
+	    	    while ((readline = br.readLine()) != null) {  
+	    	        //Log.e("readline: ", readline);
+	    	        sb.append(readline);  
+	    	    }  
+	    	    br.close(); 
+	    	    str = sb.toString();
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+    	return str;	     
+    } 
+	
+	public static double[] getCpuPercent(String fileName) {
+		String info = readLog(fileName);
+        String eL = "\\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{3}\\]\\w*:";		
+        Pattern p = Pattern.compile(eL);
+        String[] arr = p.split(info);
+        String[] arrPidCpuTime = new String[(arr.length - 1)/2];
+        String[] arrCpuTotalTime = new String[(arr.length - 1)/2];
+        double[] arrPercent = new double[arrPidCpuTime.length - 1];
+        long pidCpuTime,cpuTotalTime;
+        int k = 1;
+        for (int i = 0; i < arrPidCpuTime.length; i++) {
+			arrPidCpuTime[i] = arr[k];
+			k += 2;
+		}
+        k = 2;
+        for (int i = 0; i < arrPidCpuTime.length; i++) {
+			arrCpuTotalTime[i] = arr[k];
+			k += 2;
+		}
+        for (int i = 0; i < arrPercent.length; i++) {
+			pidCpuTime = Long.parseLong(arrPidCpuTime[i+1]) - Long.parseLong(arrPidCpuTime[i]);
+			cpuTotalTime = Long.parseLong(arrCpuTotalTime[i+1]) - Long.parseLong(arrCpuTotalTime[i]);
+			float f = (float)pidCpuTime/cpuTotalTime;
+			arrPercent[i] = getPercent(f);
+		}      
+		return arrPercent;
+	}
+}
